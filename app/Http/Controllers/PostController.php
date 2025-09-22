bad52fa37c42cbc243a31b7cb48c24db0e55758f
sigmasi2 <654bendi@gmail.com>
2025-09-22 13:31:20 +0200

    3.5
    folio

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,25 +1,28 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     //GET KÉRÉSHEZ TARTOZÓ FUNCKIÓ
 
-    public function show($post){
-         $posts=[
-        "first-post"=>"Ez az elso posztom, nagyon jo poszt!",
-        "second-post"=>"Ez a masodik posztom, ez nem jobb mint az elso!"
-    ];
-    if (!array_key_exists($post,$posts))
+    public function show($post)
     {
-        abort(404);
-    }
-       // return $post;
-         return view('post')->with([
-        'post'=>$posts[$post] ?? "EZ még nem létezik!"
-    ]);
+        //   $posts=[
+        //  "first-post"=>"Ez az elso posztom, nagyon jo poszt!",
+        //  "second-post"=>"Ez a masodik posztom, ez nem jobb mint az elso!"
+        //];
+        //if (!array_key_exists($post,$posts))
+        //{
+        //    abort(404);
+        //}
+        // return $post;
+        //    return view('post')->with([
+        //  'post'=>$posts[$post] ?? "EZ még nem létezik!"
+        //]);
+        $length = Post::getLength($post);
+        return view('post', compact('post', 'length'));
     }
 }
---
+++ resources/views/pages/projects.blade.php
@@ -0,0 +1,3 @@
+<div>
+<h1>My Projects</h1>
+</div>
